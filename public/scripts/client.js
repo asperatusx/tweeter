5b1d5ddc76ac2968e014c983a1d0a1d16d848dbd
Andrew <andrewlang.p@gmail.com>
2024-11-06 17:23:29 -0500

    create a function to render all the tweets from hardcorded data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -17,17 +17,30 @@ $(document).ready(function () {
 
   })
 
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  }
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
   
   const createTweetElement = function ({ user, content, created_at }) {
     const convertedDate = new Date(created_at * 1000);
@@ -51,5 +64,12 @@ $(document).ready(function () {
         </article>`)
   }
 
-  
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('.main-tweets').append($tweet);
+    }
+  }
+
+  renderTweets(data);
 })
