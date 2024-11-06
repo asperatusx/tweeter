619cdc2177f875f9a6a696770d42e49c92fb2a02
Andrew <andrewlang.p@gmail.com>
2024-11-06 17:15:42 -0500

    create function to render a tweeter article using jquery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,16 +4,52 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function() {
-  $('.form-container').on('submit', function(e) {
+$(document).ready(function () {
+  $('.form-container').on('submit', function (e) {
     e.preventDefault();
     const serialData = $(this).serialize();
     console.log(serialData);
-    $.ajax('/tweets', { method: 'POST', data: serialData})
-      .then(function(res) {
+    $.ajax('/tweets', { method: 'POST', data: serialData })
+      .then(function (res) {
         console.log('Its a sucess!');
         console.log(res)
       })
 
   })
+
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  }
+  
+  const createTweetElement = function ({ user, content, created_at }) {
+    const convertedDate = new Date(created_at * 1000);
+    return $(`<article>
+          <header class="article-header">
+            <div class="header-icon-container">
+              <img class="article-header-img" src=${user.avatars} alt="Face icons created by Freepik - Flaticon">
+              <p class="article-header-name">${user.name}</p>
+            </div>
+            <p class="article-header-at">${user.handle}</p>
+          </header>
+          <p class="article-content">${content.text}</p>
+          <footer class="article-footer">
+            <p class="article-footer-day">${convertedDate}</p>
+            <div class="article-footer-icons">
+              <i class="fa-solid fa-flag"></i>
+              <i class="fa-solid fa-retweet"></i>
+              <i class="fa-solid fa-heart"></i>
+            </div>
+          </footer>
+        </article>`)
+  }
+
+  
 })
