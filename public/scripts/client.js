7386470a0eb9f486c955233be626c98e7a36b014
Andrew <andrewlang.p@gmail.com>
2024-11-06 16:41:59 -0500

    create ajax post request for sending a new tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,16 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  $('.form-container').on('submit', function(e) {
+    e.preventDefault();
+    const serialData = $(this).serialize();
+    console.log(serialData);
+    $.ajax('/tweets', { method: 'POST', data: serialData})
+      .then(function(res) {
+        console.log('Its a sucess!');
+        console.log(res)
+      })
+
+  })
+})
