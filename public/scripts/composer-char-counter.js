6064378de526fc525b9b785f4e4acf053f6a7250
Andrew <andrewlang.p@gmail.com>
2024-11-05 20:06:20 -0500

    Use jquery to change counter on input and color when below limit

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("#tweet-text").on('input', function() {
+    const counter = $(this).closest('.form-container').find('.counter');
+    $(counter).val(140)
+    const value = $(counter).val() - this.value.length;
+    if (value < 0) {
+      $(counter).val(value);
+      $(counter).css('color', 'red');
+    } else {
+      $(counter).val(value);
+      $(counter).css('color', 'black');
+    }
+  })
+})
+
